d950cfb84fd7bf31b92135f21a5664280cb694e5
oceanxdds <oceanxdds@gmail.com>
2020-04-28 00:19:51 +0800

    add skill 'Null Mortal'

--- src/js/app.vue
+++ src/js/app.vue
@@ -23,7 +23,7 @@ export default {
     data:function(){
         return {
 
-        updated_at:'200423',
+        updated_at:'200428',
         //modal
         modal_id:'modal_devil_info',
         //builder
