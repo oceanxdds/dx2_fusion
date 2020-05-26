dc6c01165599ca528ec4f68ddfebf21c1399e2bb
oceanxdds <oceanxdds@gmail.com>
2020-05-27 00:05:40 +0800

    add 4 devils

--- src/js/app.vue
+++ src/js/app.vue
@@ -23,7 +23,7 @@ export default {
     data:function(){
         return {
 
-        updated_at:'200517',
+        updated_at:'200526',
         //modal
         modal_id:'modal_devil_info',
         //builder
